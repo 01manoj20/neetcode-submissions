2f5023bf4aa3f0069819adba62efdeb8ec60b570
Venkataseetharam Manoj Malipeddi <58586620+01manoj20@users.noreply.github.com>
2026-08-27 16:27:34 -0400

    Add: design-word-search-data-structure - submission-1

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-1.cpp
@@ -0,0 +1,69 @@
+class Vertex
+{
+    public:
+        int next[26];
+        bool output;
+        Vertex()
+        {
+            for(int i=0;i<26;i++)
+                next[i] = -1;
+            output = false;
+        }
+
+};
+
+class WordDictionary {
+public:
+    vector<Vertex> root;
+    WordDictionary() {
+        root.emplace_back();
+    }   
+    
+    void addWord(string word) {
+        int v = 0;
+        for(auto ch: word)
+        {
+            int c = ch-'a';
+            if(root[v].next[c]==-1)
+            {
+                root[v].next[c] = root.size();
+                root.emplace_back();
+            }   
+            v = root[v].next[c];
+        }
+        root[v].output = true;
+    }
+
+    bool search_helper(string word,int ind, int v)
+    {
+        char ch = word[ind];
+
+        if(ind == word.size())
+            return root[v].output;
+
+        if(ch == '.')
+        {
+            for(int k=0;k<26;k++)
+            {
+                if(root[v].next[k]!=-1)
+                {
+                    if(search_helper(word,ind+1,root[v].next[k]))
+                        return true;
+                }
+            }
+
+            return false;
+        }
+        else{
+            int c = word[ind] - 'a';
+            if(root[v].next[c] == -1)
+                return false;
+
+            return search_helper(word,ind+1,root[v].next[c]);
+        }
+    }
+    
+    bool search(string word) {
+        return search_helper(word,0,0);
+    }
+};
